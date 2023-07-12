bcef52b55e187214aa1c6f86d1ac0dfa81d996b5
dmswns0147@naver.com <dmswns0147@naver.com>
2023-07-12 14:42:11 +0900

    Initial Commit

---
+++ SimpleChat/SimpleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatApp.swift
+//  SimpleChat
+//
+//  Created by Nicode . on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
